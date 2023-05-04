d7efe81b9cea82d9425fde7e5e74ba382e8e380f
golee.hyper-cloud <FLzhdms147^^>
2023-05-04 16:00:05 +0900

    UPDATE: 메타데이터 업데이트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,33 @@
-import './globals.css';
-import { Open_Sans } from 'next/font/google';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import "./globals.css"
+import { Open_Sans } from "next/font/google"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 
-const sans = Open_Sans({ subsets: ['latin'] });
+const sans = Open_Sans({ subsets: ["latin"] })
 
 export const metadata = {
-  title: {
-    default: '엘리의 블로그',
-    template: '엘리의 블로그 | %s',
-  },
-  description: '풀스택 개발자 엘리의 블로그',
-  icons: {
-    icon: '/favicon.ico',
-  },
-};
+	title: {
+		default: "고리 블로그",
+		template: "고리 블로그 | %s",
+	},
+	description: "프론트엔드 개발자 고리 블로그",
+	icons: {
+		icon: "/favicon.ico",
+	},
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode
 }) {
-  return (
-    <html lang='en' className={sans.className}>
-      <body className='flex flex-col w-full max-w-screen-2xl mx-auto'>
-        <Header />
-        <main className='grow'>{children}</main>
-        <Footer />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={sans.className}>
+			<body className="flex flex-col w-full max-w-screen-2xl mx-auto">
+				<Header />
+				<main className="grow">{children}</main>
+				<Footer />
+			</body>
+		</html>
+	)
 }
